040b4932082108bfbc1d22dd6f661a841d86c788
Artyom91150 <artyom91150@gmail.com>
2023-08-17 19:30:14 +0300

    Initial commit

--- src/JSONParser.jl
+++ src/JSONParser.jl
@@ -1,5 +1,56 @@
 module JSONParser
 
-# Write your package code here.
+export parse_json, write_csv
+
+function fill_pairs!(result_dict, node, prev_key = "")
+    if typeof(node) <: Dict
+        for key in keys(node)
+            if prev_key != ""
+                fill_pairs!(result_dict, node[key], prev_key * "_" * key)
+            else
+                fill_pairs!(result_dict, node[key], prev_key * key)
+            end
+        end
+    else
+        if haskey(result_dict, prev_key)
+            push!(result_dict[prev_key], node)
+        else
+            result_dict[prev_key] = Vector{typeof(node)}()
+            push!(result_dict[prev_key], node)
+        end
+    end
+end
+
+function parse_json(json)
+    result = Dict()
+    
+    for obj in json
+        fill_pairs!(result, obj)
+    end
+
+    return sort(collect(result), by = x -> x[1])
+end
+
+function write_headers(io::IO, vec_pair, separator = ",")
+    print(io, join([header[1] for header in vec_pair], separator))
+end
+
+function write_values(io::IO, vec_pair, separator = ",")
+    val_n = length(vec_pair[begin][2])
+    key_n = length(vec_pair)
+    for i in 1 : val_n
+        for j in 1 : key_n
+            print(io, string(vec_pair[j][2][i]))
+            if j != key_n print(io, separator) end
+        end
+        if i != val_n print(io, "\n") end
+    end
+end
+
+function write_csv(io::IO, vec_pair, separator = ",")
+    write_headers(io, vec_pair, separator)
+    print(io, "\n")
+    write_values(io, vec_pair, separator)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using JSONParser
-using Test
+using Test, JSON
 
 @testset "JSONParser.jl" begin
-    # Write your tests here.
+    json_string = read("data.json", String)
+    test_json = JSON.parse(json_string)
+
+    sorted_res = parse_json(test_json)
+
+    open("output.csv", "w") do io
+        write_csv(io, sorted_res)
+    end
+
+    output = open(f->read(f, String), "output.csv")
+    test_output = open(f->read(f, String), "test_output.csv")
+
+    @test isequal(output, test_output)
 end
